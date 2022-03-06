11dbe7ec8d615ae299035a6fb0d8ac46e88487dc
AMDevelop <alejandro93mm@gmail.com>
2022-03-06 12:42:41 +0100

    First Steps in MongoDB and Docker

---
+++ MONGODB_DOCKERCOMPOSE/students.js
@@ -0,0 +1,129 @@
+student = {
+    "firstName": "Retha",
+    "lastName" : "Killen",
+    "email": "rethakillen@gmail.com",
+    "gender": "F",
+    "country": "Philippines",
+    "isStudentActive": false,
+    "favouriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentInBooks": 0.00
+}
+
+students = [
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Retha",
+        "lastName" : "Killen",
+        "email": "rethakillen@gmail.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    }
+]
---
+++ SPRING_MONGODB_REST-API/demo/src/main/java/com/example/demo/models/Address.java
@@ -0,0 +1,8 @@
+package com.example.demo.entities;
+
+public class Address {
+    private String country;
+    private String city;
+    private String postCode;
+    private String street;
+}
